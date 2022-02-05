2782c6aee6d0c9161010c72261c923b94b1bddcd
Astro (Naruto Uzumaki) <github@sandy007.anonaddy.com>
2022-02-05 17:39:07 +0530

    fixess

--- hosts/lambda/default.nix
+++ hosts/lambda/default.nix
@@ -120,38 +120,15 @@
   };
 
   #### SERVERS ####
-  services.nextcloud = {
-    enable = true;
-    hostName = "nextcloud.tld";
-    config = {
-      dbtype = "pgsql";
-      dbuser = "nextcloud";
-      dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
-      dbname = "nextcloud";
-      adminpassFile = "/path/to/admin-pass-file";
-      adminuser = "root";
-    };
-  };
-
   services.postgresql = {
     enable = true;
-    ensureDatabases = [ "nextcloud" ];
     ensureUsers = [
-      { name = "nextcloud";
-        ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
-      }
       { name = "naruto"; }
     ];
     authentication = pkgs.lib.mkOverride 12 ''
       local all all trust
       host all all ::1/128 trust
     '';
-  };
-    
-  # ensure that postgres is running *before* running the setup
-  systemd.services."nextcloud-setup" = {
-    requires = ["postgresql.service"];
-    after = ["postgresql.service"];
   };
 
   #### DANGER ZONE ####
